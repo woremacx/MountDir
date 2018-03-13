eff25f1466315e697ec02eb3ea4842e5a04bc27c
Ani <aniketg417@gmail.com>
2018-03-13 21:35:18 +0530

    to run the implementation

---
+++ t/mount.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"dirtodir"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		fmt.Println("ERROR")
+	}
+	mntd := flag.Arg(1)
+	mnt := flag.Arg(0)
+	LkFS := dirtodir.Linkfs(mntd)
+	dirtodir.M2(mnt, LkFS).Serve()
+}
